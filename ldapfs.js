8cccf4021d32fc4ddb8bf0b55da521b4c99956b6
gom900 <gom900@gmail.com>
2016-11-26 09:56:53 +0900

    テストコードを追加

---
+++ ldapfs.js
@@ -0,0 +1,32 @@
+var fs = require('fs');
+var filelist =[];
+
+function init(){
+  /*
+fs.readFile('./passwd.txt','utf8',function(err,text){
+  console.log('file read');
+  console.log(text);
+  filelist =text.replace(/\r/g, "").split("\n");
+  console.log(filelist[0]);
+  console.log('error');
+  console.log(err);
+  });
+  */
+text = fs.readFileSync('./passwd.txt',{ encoding:"utf8"});
+list =text.replace(/\r/g, "").split("\n");
+for (var i=0; i<list.length-1; i++){
+  entry= list[i].replace(/\r/g, "").split(":");
+	console.log('name : '+entry[0]);
+  filelist.push({
+     name: entry[0],
+     test:'test'
+   });
+ }
+};
+
+function getlist(){
+  return filelist;
+};
+
+module.exports.init = init;
+module.exports.getlist = getlist;
---
+++ test/main.js
@@ -0,0 +1,13 @@
+var assert = require("assert");
+var ldapfs = require("../ldapfs.js");
+describe("file_check", function() {
+    ldapfs.init();
+    it("should return jsmith object ", function() {
+      var flist= [];
+      flist.push ({
+            name: 'jsmith',
+            test:'test'
+        });
+    assert.deepEqual(flist, ldapfs.getlist());
+    });
+});
